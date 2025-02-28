4b0193ba4e264927dff1a5cc75b6fef62805a917
leomanangka <leomanangka@outlook.com>
2025-02-28 11:22:56 +0700

    add led indicator if the station is connected to access point and option to change publish interval using mqtt_topic_t

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include "cJSON.h"
-#include "esp_log.h"
+#include "driver/gpio.h"
 #include "freertos/idf_additions.h"
 #include "mqtt_client_wrapper.h"
 #include "network_wrapper.h"
@@ -7,12 +7,18 @@
 #include <string.h>
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define BUILT_IN_LED GPIO_NUM_2
 
 void app_main() {
-  static const char *MAIN_TAG = "[APP MAIN]";
+  gpio_set_direction(BUILT_IN_LED, GPIO_MODE_OUTPUT);
+  gpio_set_level(BUILT_IN_LED, 0);
+
   static mqtt_topic_t mqtt_topic = {
       .sub_topic = "topic/sub_test",
       .pub_topic = "topic/pub_test",
+      .interval = 10, // interval for 5s
   };
 
   cJSON *json_data = cJSON_CreateObject();
@@ -24,7 +30,15 @@ void app_main() {
 
   wifi_start();
   vTaskDelay(2000 / portTICK_PERIOD_MS);
-  mqtt_start(&mqtt_topic);
-  xTaskCreate(mqtt_pusblish_task, "publish task", 1024 * 2, &mqtt_topic, 2,
-              NULL);
+
+  if (is_sta_connected()) {
+    gpio_set_level(BUILT_IN_LED, 1);
+    mqtt_start(&mqtt_topic);
+    xTaskCreate(mqtt_pusblish_task, "publish task", 1024 * 2, &mqtt_topic, 2,
+                NULL);
+  } else {
+    cJSON_Delete(json_data);
+    cJSON_free(mqtt_topic.msg);
+    gpio_set_level(BUILT_IN_LED, 0);
+  }
 }
