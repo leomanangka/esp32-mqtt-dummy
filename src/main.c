1afa33af05e2373afc4ab3ab4a58b4c236edabd9
leomanangka <leomanangka@outlook.com>
2025-03-07 13:14:00 +0700

    add publish_task function to handle publish on RTOS, send data every 10 second and led indicator for publish task

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 #include "freertos/idf_additions.h"
 #include "mqtt_client_wrapper.h"
 #include "network_wrapper.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "freertos/FreeRTOS.h"
@@ -11,34 +13,62 @@
 
 #define BUILT_IN_LED GPIO_NUM_2
 
-void app_main() {
-  gpio_set_direction(BUILT_IN_LED, GPIO_MODE_OUTPUT);
-  gpio_set_level(BUILT_IN_LED, 0);
+static float rand_number(float min, float max) {
+  float scale = (float)rand() / RAND_MAX;
+  return min + scale * (max - min);
+}
 
+static void publish_task(void *arg) {
+  mqtt_topic_t *mqtt_topic = (mqtt_topic_t *)arg;
+  static int interval = 1000;
+  if (mqtt_topic->interval != 0) {
+    interval = interval * mqtt_topic->interval;
+  }
+
+  gpio_set_direction(BUILT_IN_LED, GPIO_MODE_OUTPUT);
+  cJSON *json_data = cJSON_CreateObject();
+  while (true) {
+    gpio_set_level(BUILT_IN_LED, 0);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    float temperature_rng = rand_number(-40, 80);
+    char temperature[64];
+    snprintf(temperature, sizeof(temperature), "%.2f", temperature_rng);
+
+    float humidity_rng = rand_number(0, 100);
+    char humidity[64];
+    snprintf(humidity, sizeof(humidity), "%.2f", humidity_rng);
+
+    float water_level_rng = rand_number(0, 100);
+    char waterl_level[64];
+    snprintf(waterl_level, sizeof(waterl_level), "%.2f", water_level_rng);
+
+    cJSON_AddStringToObject(json_data, "MCU", "esp32");
+    cJSON_AddNumberToObject(json_data, "temperature", atof(temperature));
+    cJSON_AddNumberToObject(json_data, "humidity", atof(humidity));
+    cJSON_AddNumberToObject(json_data, "water_level", atoi(waterl_level));
+
+    mqtt_topic->msg = cJSON_Print(json_data);
+
+    mqtt_pusblish(mqtt_topic);
+    gpio_set_level(BUILT_IN_LED, 1);
+    vTaskDelay(interval / portTICK_PERIOD_MS);
+  }
+  cJSON_Delete(json_data);
+  cJSON_free(mqtt_topic->msg);
+  gpio_set_level(BUILT_IN_LED, 0);
+}
+
+void app_main() {
   static mqtt_topic_t mqtt_topic = {
       .sub_topic = "topic/sub_test",
       .pub_topic = "topic/pub_test",
-      .interval = 10, // interval for 5s
+      .interval = 10, // interval for 10s
   };
-
-  cJSON *json_data = cJSON_CreateObject();
-  cJSON_AddStringToObject(json_data, "MCU", "esp32");
-  cJSON_AddNumberToObject(json_data, "node", 1);
-  cJSON_AddNumberToObject(json_data, "temperature", 30);
-
-  mqtt_topic.msg = cJSON_Print(json_data);
 
   wifi_start();
   vTaskDelay(2000 / portTICK_PERIOD_MS);
-
   if (is_sta_connected()) {
-    gpio_set_level(BUILT_IN_LED, 1);
     mqtt_start(&mqtt_topic);
-    xTaskCreate(mqtt_pusblish_task, "publish task", 1024 * 2, &mqtt_topic, 2,
-                NULL);
-  } else {
-    cJSON_Delete(json_data);
-    cJSON_free(mqtt_topic.msg);
-    gpio_set_level(BUILT_IN_LED, 0);
+    xTaskCreate(publish_task, "Publish Task", 1024 * 2, &mqtt_topic, 1, NULL);
   }
 }
